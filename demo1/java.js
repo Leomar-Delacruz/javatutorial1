32638de2bf63bc463861d7a2188c3df2245eff5e
Leomar P. De la cruz <146308573+Leomar-Delacruz@users.noreply.github.com>
2024-03-12 21:25:40 +0800

    added

--- demo1/java.js
+++ demo1/java.js
@@ -1,2 +1,9 @@
+console.log("hello");
+console.log("i like u");
+
+window.alert("This is alert");
+window.alert("This is the second alert")
+
+
 document.getElementById("test1").textContent = "This is the basic learning in javascript";
 document.getElementById("test2").textContent = "Hope you enjoy";
