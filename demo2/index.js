f402c068ba0e504d92ddd3c331598f12b5692d1e
Leomar P. De la cruz <146308573+Leomar-Delacruz@users.noreply.github.com>
2024-03-14 21:30:12 +0800

    added new topic

---
+++ demo2/index.js
@@ -0,0 +1,17 @@
+// variable = A container that stores a value
+//              Behave as if it were the values it contains.
+
+// 1. declares let x;
+// 2. assignment x = 100;
+
+let fullname= "Leomar";
+let age = 21;
+let school = "Batangas State Univertsity Lipa Branch";
+
+console.log(`My name is ${fullname} and my age is ${age} years old and my school is ${school}.`);
+
+document.getElementById("p1").textContent = `My name is ${fullname} and my age is ` + age;
+
+
+
+
