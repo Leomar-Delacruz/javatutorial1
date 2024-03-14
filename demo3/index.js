b70f8ea7a94f6f4eaccdac6ba1242fc814e3dd62
Leomar P. De la cruz <146308573+Leomar-Delacruz@users.noreply.github.com>
2024-03-14 21:56:19 +0800

    added new topic

---
+++ demo3/index.js
@@ -0,0 +1,34 @@
+// arithmetic operators = operands (values, varaibles, etc..)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+
+let students = 40;
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 1;
+students = students ** 2;
+students = students % 4;
+
+students += 1;
+students -= 1;
+students *= 1;
+students /= 1;
+students **= 1;
+students %= 1;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 5 + 2 * 10 - 5;
+
+console.log(result);
+
+
